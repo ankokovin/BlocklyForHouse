2a90482a5d652cdc9e8ed120748b79d46b582205
ankokovin <rycarok@gmail.com>
2019-04-14 17:31:52 +0500

    Попытка локализации строк

---
+++ version/0.1.1 localization - canceled/my msgs/ru.js
@@ -0,0 +1,20 @@
+//Categories
+Blockly.Msg.CATEGORY_LOGIC = "Логика";
+Blockly.Msg.CATEGORY_IF = "Условия";
+Blockly.Msg.CATEGORY_BOOLEAN = "Операции";
+Blockly.Msg.CATEGORY_LOOPS = "Циклы";
+Blockly.Msg.CATEGORY_MATH = "Математика";
+Blockly.Msg.CATEGORY_LIST = "Списки";
+Blockly.Msg.CATEGORY_TEXT = "Текст";
+Blockly.Msg.CATEGORY_ACTUATORS = "Актуаторы";
+Blockly.Msg.CATEGORY_DOOR = "Дверь";
+Blockly.Msg.CATEGORY_LAMP = "Лампа";
+Blockly.Msg.CATEGORY_SENSORS = "Датчики";
+Blockly.Msg.CATEGORY_TEMPERATURE = "Температура";
+Blockly.Msg.CATEGORY_HUMIDITY = "Влажность";
+Blockly.Msg.CATEGORY_ILLUMINANCE = "Освещённость";
+Blockly.Msg.CATEGORY_VARIABLES = "Переменные";
+Blockly.Msg.CATEGORY_PROCEDURES = "Функции";
+
+//door
+Blockly.Msg.DOOR_CREATE = "Дверь названная:";
