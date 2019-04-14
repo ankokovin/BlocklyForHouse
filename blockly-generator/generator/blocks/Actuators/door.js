8f4290a65ea33e00edb9db79c6d3ea2aefd23fa8
ankokovin <rycarok@gmail.com>
2019-04-14 19:26:17 +0500

    fixed import TODO: import all

--- blockly-generator/generator/blocks/Actuators/door.js
+++ blockly-generator/generator/blocks/Actuators/door.js
@@ -1,26 +1,26 @@
+module.exports = function(Blockly) {
+  Blockly.Blocks['door'] = {
+    init: function() {
+      this.appendValueInput("NAME")
+      .setCheck("String")
+      .appendField("Door Named:");
+      this.setOutput(true, "door");
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
 
-Blockly.Blocks['door'] = {
-  init: function() {
-    this.appendValueInput("NAME")
-        .setCheck("String")
-        .appendField("Door Named:");
-    this.setOutput(true, "door");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['open_door'] = {
-  init: function() {
+  Blockly.Blocks['open_door'] = {
+    init: function() {
     this.appendValueInput("door")
-        .setCheck("door")
-        .appendField("Open door");
+    .setCheck("door")
+    .appendField("Open door");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
@@ -29,12 +29,12 @@ Blockly.Blocks['close_door'] = {
     this.appendValueInput("door")
         .setCheck("door")
         .appendField("Close door");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
  this.setHelpUrl("");
-  }
+}
 };
 
 Blockly.Blocks['door_is_open'] = {
@@ -43,9 +43,11 @@ Blockly.Blocks['door_is_open'] = {
         .setCheck("door")
         .setAlign(Blockly.ALIGN_CENTRE)
         .appendField("Is open");
-    this.setOutput(true, "Boolean");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+        this.setOutput(true, "Boolean");
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+      }
+    };
+    
+}
--- blockly-generator/generator/generator.js
+++ blockly-generator/generator/generator.js
@@ -1,86 +1,9 @@
 var Blockly = require('node-blockly');
 
-Blockly.Blocks['door'] = {
-  init: function() {
-    this.appendValueInput("NAME")
-        .setCheck("String")
-        .appendField("Door Named:");
-    this.setOutput(true, "door");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+require('./blocks/Actuators/door')(Blockly)
 
-Blockly.Blocks['open_door'] = {
-  init: function() {
-    this.appendValueInput("door")
-        .setCheck("door")
-        .appendField("Open door");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+require('./generators/python/Actuators/door')(Blockly)
 
-Blockly.Blocks['close_door'] = {
-  init: function() {
-    this.appendValueInput("door")
-        .setCheck("door")
-        .appendField("Close door");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['door_is_open'] = {
-  init: function() {
-    this.appendValueInput("door")
-        .setCheck("door")
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("Is open");
-    this.setOutput(true, "Boolean");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-
-Blockly.Python['door'] = function(block) {
-  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  var code = 'door('+value_name+')';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Python.ORDER_NONE];
-};
-
-Blockly.Python['open_door'] = function(block) {
-  var value_door = Blockly.Python.valueToCode(block, 'door', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  var code = value_door+'.open()\n';
-  return code;
-};
-
-Blockly.Python['close_door'] = function(block) {
-  var value_door = Blockly.Python.valueToCode(block, 'door', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  var code = value_door+'.close()\n';
-  return code;
-};
-
-Blockly.Python['door_is_open'] = function(block) {
-  var value_door = Blockly.Python.valueToCode(block, 'door', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
-  var code = value_door + '.is_open()';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.Python.ORDER_NONE];
-};
 const fs = require('fs');
 filename = 'input.txt';
 if (process.argv.length>2)
