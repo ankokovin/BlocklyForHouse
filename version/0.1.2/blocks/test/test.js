dce71f13ea737c9c6d5381c2fb51ddef267ecb66
ankokovin <lena13u@yandex.ru>
2019-09-26 20:32:24 +0500

    add block

---
+++ version/0.1.2/blocks/test/test.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['test'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("test");
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+  
---
+++ version/0.1.2/generators/python/test/test.js
@@ -0,0 +1,5 @@
+Blockly.Python['test'] = function(block) {
+    // TODO: Assemble Python into code variable.
+    var code = 'print("test")\n';
+    return code;
+  };
