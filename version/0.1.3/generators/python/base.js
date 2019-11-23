e32abc7e4c214c5d73d1ff562e95dcda12b969e3
ankokovin <rycarok@gmail.com>
2019-11-23 14:06:08 +0500

    delete script block

--- version/0.1.3/generators/python/base.js
+++
@@ -1,8 +0,0 @@
-Blockly.Python['script'] = function(block) {
-    var statements_body = Blockly.Python.statementToCode(block, 'BODY');
-    var code = 'def script():'
-      + (statements_body.length>0?'\n'+statements_body:'\n    pass')
-      +'\n'
-      +'script()';
-    return code;
-  };
